dfcaae0649c0414d9bc4fe16d2c5b576a8f2b5c9
Havrushchenko <glitteringpuzzlemusic@gmail.com>
2023-02-15 12:35:22 +0200

    added the ability to conditionally render gallery pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,7 @@ function About() {
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis </p>
     </section>
   );
 }
